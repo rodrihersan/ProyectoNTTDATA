b82170d3f7dce82783fa5c099f48fb5fd5cf8a02
miguelGutierrez373 <miguelgc373@gmail.com>
2026-05-21 13:47:01 +0200

    Subida proyecto limpieza

---
+++ codeMages/codeMage-backend-v2/src/main/java/com/codemage/CodeMageApplication.java
@@ -0,0 +1,12 @@
+package com.codemage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeMageApplication.class, args);
+    }
+}
